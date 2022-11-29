8180c3ba64d4b69ae146fcd285c5aec5cda8a867
farrokh <saytec@gmail.com>
2022-11-29 01:31:19 -0800

    head bug fix

--- components/heads/IndexHead.tsx
+++ components/heads/IndexHead.tsx
@@ -49,9 +49,7 @@ const IndexHead: React.FC = () => {
       {/* html */}
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name="description" content={text.general.headers.description} />
-      <title>
-        {text.home.title} - {text.general.name}
-      </title>
+      <title>{`${text.home.title} - ${text.general.name}`}</title>
       <MetaLinks />
     </Head>
   );
